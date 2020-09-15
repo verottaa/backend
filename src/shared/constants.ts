0a1a3c534c88e756b553c5246240ebb3a39a9c4e
Золотов Никита <zolotov_ns@teleformis.ru>
2020-09-15 21:02:53 +0300

    удалил старый проект

--- src/shared/constants.ts
+++
@@ -1,6 +0,0 @@
-export class Constants {
-  public static globalPrefix = 'api/v1';
-
-  public static DATABASE_CONNECTION = 'DATABASE_CONNECTION';
-  public static USER_REPOSITORY = 'USER_REPOSITORY';
-}
